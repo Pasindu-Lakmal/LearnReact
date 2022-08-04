0dad9f6248b7a610a8e62a248b895077dd1d3d2c
Pasindu-Lakmal <pasindu1lak@gmail.com>
2022-08-04 17:52:46 +0530

    first component

---
+++ first-app/src/componrnts/Greet.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+// function Greet() {
+//   return <h1>Hello Pasindu</h1>;
+// }
+
+// use arrow function for same thing
+
+const Greet = () => <h1>Hello Pasindu</h1>;
+
+export default Greet;
