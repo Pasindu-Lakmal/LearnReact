21b3446d7e8fed7376ae9b016393a8274666b359
Pasindu-Lakmal <pasindu1lak@gmail.com>
2022-08-04 19:00:46 +0530

    props

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,11 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Greet from "./componrnts/Greet";
+import Welcome from "./componrnts/Welcome";
+import Hello from "./componrnts/Hello";
 
 function App() {
   return (
     <div className="App">
-      <Greet />
+      {/* <Greet />
+      <Welcome /> */}
+      <Hello />
     </div>
   );
 }
---
+++ first-app/src/componrnts/Welcome.js
@@ -0,0 +1,11 @@
+// create class component
+
+import React, { Component } from "react";
+
+class Welcome extends Component {
+  render() {
+    return <h1>Class component</h1>;
+  }
+}
+
+export default Welcome;
